dfbdcb8cfac98a6e1993b2e98ccaa331eb4933cd
christian-cunanan-caparra <caparrachristian8@gmail.com>
2025-03-20 10:22:47 +0800

    pusher

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,52 +1,50 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-
-
-
-
-
-
-
 android {
     namespace = "com.example.members_lists"
-    compileSdk = flutter.compileSdkVersion
-//    ndkVersion = flutter.ndkVersion
-    ndkVersion = "27.0.12077973"
+    compileSdk 34 // Use actual SDK version
+
+    ndkVersion "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility JavaVersion.VERSION_11
+                targetCompatibility JavaVersion.VERSION_11
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "11"
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.members_lists"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        applicationId "com.example.members_lists"
+        minSdk 21  // Set this according to your app's needs
+        targetSdk 34
+        versionCode 1
+        versionName "1.0.0"
+    }
+
+    signingConfigs {
+        release {
+            storeFile file("app/keystore.jks") // Correct path
+            storePassword System.getenv("CM_KEYSTORE_PASSWORD") ?: "123456789"
+            keyAlias System.getenv("CM_KEY_ALIAS") ?: "my-key"
+            keyPassword System.getenv("CM_KEY_PASSWORD") ?: "123456789"
+        }
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig signingConfigs.release
+                    minifyEnabled false
+            shrinkResources false
         }
     }
 }
 
 flutter {
-    source = "../.."
+    source "../.."
 }
